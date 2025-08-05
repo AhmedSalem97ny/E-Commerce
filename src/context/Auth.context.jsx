1d64c38ddf12aae6f04aded0a02486bb9e3336c6
Ahmed Salem <ahmedsalem97ny@gmail.com>
2025-08-05 18:41:54 +0300

    Update Auth.context.jsx

--- src/context/Auth.context.jsx
+++ src/context/Auth.context.jsx
@@ -1,19 +1,44 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { verifyToken } from "../services/auth-service";
 
 export const Authcontext = createContext(null);
 
-export default function Authprovider({children}){
+export default function Authprovider({ children }) {
+  const [token, setToken] = useState(
+    localStorage.getItem("token") || sessionStorage.getItem("token")
+  );
 
 
-    const [token, setToken]= useState(localStorage.getItem("token") || sessionStorage.getItem("token"))
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+const[userInfo, setUserInfo] = useState(null);
 
-function logOut(){
+  useEffect(() => {
+  const checkAuthentication= async ()=>{
+     try {
+    const response= await verifyToken();
+    if(response.success){
+        setIsAuthenticated(true);
+        setUserInfo(response.data.decoded);
+    }
+    
+   } catch (error) {
+    console.log(error);
+    
+   }
+  }
+ checkAuthentication()
+  },[])
+
+  function logOut() {
     setToken(null);
     localStorage.removeItem("token");
     sessionStorage.removeItem("token");
+  }
+
+  return (
+    <Authcontext.Provider value={{ token, setToken, logOut, isAuthenticated, userInfo }}>
+      {children}
+    </Authcontext.Provider>
+  );
 }
 
-    return <Authcontext.Provider value={{token,setToken, logOut}}>
-{children}
-    </Authcontext.Provider>
-}
