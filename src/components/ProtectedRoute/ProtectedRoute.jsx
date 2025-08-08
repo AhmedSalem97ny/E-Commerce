138ef2d96e761a5db3a523fc503a29dffa7f6222
Ahmed Salem <ahmedsalem97ny@gmail.com>
2025-08-08 17:28:33 +0300

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,19 @@
 import { useContext } from "react";
 import { Authcontext } from "../../context/Auth.context";
 import { Navigate, useLocation } from "react-router";
+import Loading from "../Loading/Loading";
 
 export default function ProtectedRoute({children}){
+   
+   const {isAuthenticated, isLoading}=useContext(Authcontext)
+   
     const location= useLocation();
-    const {token}=useContext(Authcontext)
+    
+    if(isLoading){
+        return <Loading/>
+    }
 
-    if(token ===null){
+    if(!isAuthenticated){
         return <Navigate to="/login" state={{from: location.pathname}} />
     }else {
          return children
