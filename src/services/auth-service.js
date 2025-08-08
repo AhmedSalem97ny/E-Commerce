fc66a9fc7c2b94f82b3a15b818fd04c9dcf83e72
Ahmed Salem <ahmedsalem97ny@gmail.com>
2025-08-08 17:29:11 +0300

    Update auth-service.js

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -63,6 +63,22 @@ export function resetPassword({ email, newPassword }) {
 
 
 
+export async function verifyToken() {
+  try {
+    const options = {
+      method: "GET",
+      url: "/auth/verifyToken",
+    };
+     const response = await apiClient.request(options);
+     return response
+  } catch (error) {
+   throw error
+  }
+}
+
+
+
+
 
 
 
