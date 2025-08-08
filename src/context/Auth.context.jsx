3c512c033d63cb24eb46bd16ed037480f667b66c
Ahmed Salem <ahmedsalem97ny@gmail.com>
2025-08-08 17:28:00 +0300

    Update Auth.context.jsx

--- src/context/Auth.context.jsx
+++ src/context/Auth.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import { verifyToken } from "../services/auth-service";
 
 
 export const Authcontext = createContext(null);
@@ -7,21 +8,49 @@ export default function Authprovider({ children }) {
   const [token, setToken] = useState(
     localStorage.getItem("token") || sessionStorage.getItem("token")
   );
+  const [isLoading, setIsLoading] = useState(true);
 
+const[isAuthenticated, setIsAuthenticated] = useState(false);
+const [userInfo, setUserInfo] = useState(JSON.parse(localStorage.getItem("userInfo")));
 
+useEffect( ()=>{
+  const checkAuth = async () => {
+    try {
+      setIsLoading(true);
+      const response = await verifyToken()
+      if(response.success){
+        setIsLoading(false);
+        setIsAuthenticated(true);
+        setUserInfo(response.data.decoded)
+        localStorage.setItem("userInfo", JSON.stringify(response.data.decoded));
+      }
+    } catch (error) {
+console.log(error);
+      setIsLoading(false);
+    }
+
+  }
+  checkAuth();
+},[token])
  
 
   function logOut() {
     setToken(null);
+    setUserInfo(null);
+    setIsAuthenticated(false);
     localStorage.removeItem("token");
+    localStorage.removeItem("userInfo");
+   sessionStorage.removeItem("userInfo");
     sessionStorage.removeItem("token");
   }
 
   return (
-    <Authcontext.Provider value={{ token, setToken, logOut}}>
+    <Authcontext.Provider value={{ token, setToken, logOut, isAuthenticated, userInfo, isLoading}}>
       {children}
     </Authcontext.Provider>
   );
 }
+
+
 
 
